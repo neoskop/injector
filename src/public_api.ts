ce2464c3d5032c8a24ed2c739c367c337c3e5915
Mark Wecke <wecke@neoskop.de>
2018-03-01 13:27:35 +0100

    feat(): add reexport of Injectable and InjectionToken from angular core

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,4 +1,4 @@
 export * from './factory';
 export * from './private_api';
 
-export { Injector, StaticProvider, Inject, Optional, Host, Self, SkipSelf } from '@angular/core';
+export { Injector, StaticProvider, Inject, Optional, Host, Self, SkipSelf, Injectable, InjectionToken } from '@angular/core';
